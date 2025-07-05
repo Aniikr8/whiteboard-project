01e129fd60c70a648c3b7680d8d77043b1e179ef
Anirudh Kumar <74367440+Aniikr8@users.noreply.github.com>
2025-07-05 23:45:19 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,20 +1,36 @@
-const mysql = require("mysql");
+// const mysql = require("mysql");
 
-const db = mysql.createConnection({
+// const db = mysql.createConnection({
+//   host: "sql12.freesqldatabase.com",
+//   user: "sql12788505",
+//   password: "xyBmsiJ7iD",
+//   database: "sql12788505",
+//   port: 3306,
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     console.error("❌ Error connecting to MySQL:", err);
+//     return;
+//   }
+//   console.log("✅ Connected to MySQL database");
+// });
+
+// module.exports = db;
+// db.js
+const mysql = require("mysql2");
+
+const pool = mysql.createPool({
   host: "sql12.freesqldatabase.com",
   user: "sql12788505",
   password: "xyBmsiJ7iD",
   database: "sql12788505",
   port: 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error("❌ Error connecting to MySQL:", err);
-    return;
-  }
-  console.log("✅ Connected to MySQL database");
-});
+module.exports = pool;
 
-module.exports = db;
 
