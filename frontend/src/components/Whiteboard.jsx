23343f1a1fecc2dcaa1d2dcb3ed9178487043bee
Anirudh Kumar <74367440+Aniikr8@users.noreply.github.com>
2025-07-06 13:29:20 +0530

    Update Whiteboard.jsx

--- frontend/src/components/Whiteboard.jsx
+++ frontend/src/components/Whiteboard.jsx
@@ -195,7 +195,7 @@ const Whiteboard = () => {
 
       <canvas
         ref={canvasRef}
-        className="absolute top-0 left-0 w-full h-full"
+        className="absolute top-0 left-0 w-full h-full touch-none"
         onMouseDown={startDrawing}
         onMouseMove={draw}
         onMouseUp={stopDrawing}
